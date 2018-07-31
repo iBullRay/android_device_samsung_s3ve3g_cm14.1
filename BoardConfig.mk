f7dca91ad7987b65fea58ff20c4669126329677b
iBullRay <bullray097@yandex.ru>
2018-07-31 19:58:39 +0300

    s3ve3gxx: Remove unused makefile flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,6 @@ DEVICE_PATH := device/samsung/s3ve3gxx
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_s3ve3gxx
-TARGET_RECOVERY_DEVICE_MODULES := libinit_s3ve3gxx
 TARGET_OTA_ASSERT_DEVICE := s3ve3g,s3ve3gds,s3ve3gjv,s3ve3gxx
 
 # Kernel
