9287340ef447d9e526bd71036c379c0dd63070f1
Unknown <bullray097@yandex.ru>
2018-09-16 21:34:12 +0300

    Revert "s3ve3g: Add nfc-brcm"
    This reverts commit 68f01bd9e1555543e204ada7ef15c9c0db7c8463.

--- device.mk
+++ device.mk
@@ -41,7 +41,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    $(DEVICE_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     $(DEVICE_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
     $(DEVICE_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml
 
